3794dbfaf2e86a0bb9902a3b06462b88c7a1b125
Kim Burgess <kim@acaprojects.com>
2017-03-27 20:09:36 +1000

    implement read and write from chrome storage

---
+++ src/js/config.js
@@ -0,0 +1,28 @@
+const storage = chrome.storage.local;
+
+module.exports = {
+
+  set: (key, value) => new Promise((resolve, reject) => {
+    const setting = {};
+    setting[key] = value;
+    storage.set(setting, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError.string);
+      } else {
+        resolve(value);
+      }
+    })
+  }),
+
+  get: key => new Promise((resolve, reject) => {
+    storage.get(key, items => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError.string);
+      } else {
+        console.log(items);
+        resolve(items[key]);
+      }
+    })
+  })
+
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,25 @@
+import * as config from './config';
 import '../css/main.css';
 
-document.body.onload = createWebview('http://www.acaprojects.com');
 
-function createWebview(url) {
+function showSetup() {
+  config.set('url', 'http://www.example.com')
+}
+
+function showWebview(url) {
   const webview = document.createElement('webview');
   webview.setAttribute('src', url);
-
   document.body.appendChild(webview);
 }
+
+function renderUI() {
+  config.get('url').then(url => {
+    if (url) {
+      showWebview(url);
+    } else {
+      showSetup();
+    }
+  });
+}
+
+document.body.onload = renderUI;
