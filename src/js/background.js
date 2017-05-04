83f49d422ba7874354b1551a716cae4579c14272
Kim Burgess <kim@acaprojects.com>
2017-05-04 18:09:26 +1000

    add keyboard shortcut for exiting the app

--- src/js/background.js
+++ src/js/background.js
@@ -7,3 +7,20 @@ chrome.app.runtime.onLaunched.addListener(() => {
   chrome.power.requestKeepAwake("display");
 
 });
+
+
+chrome.commands.onCommand.addListener(command => {
+
+  const handlers = {
+    'exit-app': exitApp
+  };
+
+  const defaultHandler = () => undefined;
+  const handler = cmd => handlers[cmd] || defaultHandler;
+
+  handler(command).apply();
+
+});
+
+const closeWindow = win => win.close();
+const exitApp = () => chrome.app.window.getAll().forEach(closeWindow);
